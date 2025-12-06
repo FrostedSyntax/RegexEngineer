49b83f63f4fea1b2911544f230c047e56db6b6fd
Christian Frost <frostedsyntax@gmail.com>
2025-12-05 18:27:07 -0600

    Added RegexParser class

---
+++ RegexEngineer/PatternComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace RegexEngineerLib
+{
+    public struct PatternComponent
+    {
+        public string Type { get; set; }
+
+        public string Value { get; set; }
+
+        public string Description { get; set; }
+
+        public int Position { get; set; }
+    }
+}
